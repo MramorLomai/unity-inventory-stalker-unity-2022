d6205af4ebef0d2bb687045960fdc781bae63d66
AlexanderDrobov <113101576+MramorLomai@users.noreply.github.com>
2026-04-22 15:48:05 +0300

    Delete Script directory

--- Script/Inventory/InventoryTypes.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public static class InventoryTypes
-{
-    public enum InventoryItemSize { x64x64, x128x64, x128x128, x256x128 }
-    public enum InventoryItemType { Passive, ActiveToClick, ActiveOne, ActiveTwo, ActiveThree, ActiveFour }
-}
